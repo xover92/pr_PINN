a0810e7995fe8858c8139d83d5dbf3014648db4d
xover92 <francesco.colombo_03@libero.it>
2026-07-07 15:43:28 +0200

    [FEAT] Added 3d PINN with similar logic to 2d. Added the 3d PINN with naive sampling and boundary conditions, no tests and no benchmarking.

--- pr_PINN/__main__.py
+++ pr_PINN/__main__.py
@@ -4,6 +4,8 @@
 import pr_PINN.pr_pinn as prp
 import torch
 import gradio as gr
+import pr_PINN.pr_prinn2d as prp2
+import pr_PINN.pr_pinn3d as prp3
 
 __author__ = ['Francesco Colombo']
 __email__ = ['francesco.colombo10@studio.unibo.it']
@@ -11,9 +13,20 @@ __email__ = ['francesco.colombo10@studio.unibo.it']
 torch.manual_seed(42)
 
 
+def choose_dimensions(n_epochs, n_neurons, n_dimensions):
+    if n_dimensions == 1:
+        fig = prp.generate_plot_1d(n_epochs, n_neurons)
+    if n_dimensions == 2:
+        fig = prp2.generate_plot_2d(n_epochs, n_neurons)
+    if n_dimensions == 3:
+        fig = prp3.generate_plot_3d(n_epochs, n_neurons)
+    return fig
+
+
 demo = gr.Interface(
-    fn=prp.generate_plot_1d,
-    inputs=[gr.Slider(maximum=1000), gr.Slider()],
+    fn=choose_dimensions,
+    inputs=[gr.Slider(maximum=1000), gr.Slider(),
+            gr.Slider(minimum=1, maximum=3, step=1.0)],
     outputs=gr.Plot(),
     api_name="predict"
 )
